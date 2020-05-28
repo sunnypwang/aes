c579c7540901b7f528ef024880526edf9596f871
Phakawat Wangkriangkri <mariosunshinekirby@gmail.com>
2020-05-28 20:42:39 +0700

    using generator

--- data_utils.py
+++ data_utils.py
@@ -5,7 +5,6 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from keras.preprocessing.sequence import pad_sequences
 
 import utils
-import eval_utils
 
 augment_set = ['no_art', 'no_conj', 'add_and_0.1', 'swap_0.05',
                'no_first_sent', 'no_last_sent', 'no_longest_sent', 'reverse_sent']
@@ -13,6 +12,43 @@ augment_set = ['no_art', 'no_conj', 'add_and_0.1', 'swap_0.05',
 MAXLEN = [-1, 70, 88, 22, 23, 24, 20, 67, 97]
 
 PAD_SENT_TOKEN = ''
+
+score_range = [(-1, -1),
+               (2, 12),
+               (1, 6),
+               (0, 3),
+               (0, 3),
+               (0, 4),
+               (0, 4),
+               (0, 30),
+               (0, 60)]
+
+
+def get_threshold(p):
+    low, high = score_range[p]
+    return 1/((high - low))
+
+
+def rescale_to_int(raw, p):
+    assert (raw >= 0.).all() and (raw <= 1.).all()
+    low, high = score_range[p]
+    return np.around(raw * (high - low) + low).astype(int)
+
+
+def normalize_score(Y, p):
+    low, high = score_range[p]
+    Y = np.array(Y)
+    Y_norm = (Y - low)/(high - low)
+    assert (Y_norm >= 0.).all() and (Y_norm <= 1.).all()
+    Y_resolved = rescale_to_int(Y_norm, p)
+    try:
+        assert np.equal(Y, Y_resolved).all()
+    except AssertionError:
+        for i in range(len(Y)):
+            if Y[i] != Y_resolved[i]:
+                print(i, Y[i], Y_resolved[i])
+        print('use python3')
+    return Y_norm
 
 
 def clean_text(text):
@@ -56,16 +92,27 @@ def sentenize(text):
 #             MAX_LEN[prompt] = len(essay)
 
 
-def prepare_elmo_features(df, prompt, features='essay', labels='domain1_score'):
-    X_tmp = []
-    for essay in df[features]:
-        X_tmp.append(sentenize(essay))
-    X_tmp = np.array(X_tmp)
-    X_pad = pad_sequences(X_tmp, maxlen=MAXLEN[prompt], dtype=object,
-                          padding='post', value=PAD_SENT_TOKEN)[:, :, None]
-    Y = np.array(df[labels].tolist())
-    Y_norm = eval_utils.normalize_score(Y, prompt)
-    return X_pad, Y_norm
+def prepare_elmo_features(df, prompt, features='essay', labels='domain1_score', x_only=False, y_only=False, norm=True):
+    assert not (x_only and y_only)
+    if not y_only:
+        X_tmp = []
+        for essay in df[features]:
+            X_tmp.append(sentenize(essay))
+        X_tmp = np.array(X_tmp)
+        X_pad = pad_sequences(X_tmp, maxlen=MAXLEN[prompt], dtype=object,
+                              padding='post', value=PAD_SENT_TOKEN)[:, :, None]
+        if x_only:
+            return X_pad
+    if not x_only:
+        Y = np.array(df[labels].tolist())
+
+        if norm:
+            Y = normalize_score(Y, prompt)
+
+        if y_only:
+            return Y
+
+    return X_pad, Y
 
 
 def load_data(prompt, suffix=None, fold=1):
@@ -84,13 +131,17 @@ def load_elmo_features(prompt, suffix=None, fold=1):
     return X, Y
 
 
-def elmo_gen(prompt, suffix, fold=1, batch_size=1):
-    data = load_data(prompt, suffix, fold)
+def elmo_gen(prompt, df, batch_size=1, test=False):
+    data = df.copy()
     while True:
         for i in range(0, len(data), batch_size):
             j = min(len(data), i+batch_size)
-            x, y = prepare_elmo_features(data[i:j], prompt)
-            yield x, y
+            if test:
+                x = prepare_elmo_features(data[i:j], prompt, x_only=True)
+                yield x
+            else:
+                x, y = prepare_elmo_features(data[i:j], prompt)
+                yield x, y
 
 
 # def load_data(prompt_id, fold_id, suffix):
@@ -105,5 +156,3 @@ def clean_data(df):
     for essay in df:
         new_df.append(clean_text(essay))
     return new_df
-
-
--- elmo_train.py
+++ elmo_train.py
@@ -11,6 +11,7 @@ import models
 parser = argparse.ArgumentParser()
 parser.add_argument('prompt', type=int, help='-1 for all prompts')
 parser.add_argument('epoch', type=int)
+parser.add_argument('--bs', type=int, default=5)
 parser.add_argument('--fold', type=int, default=1)
 parser.add_argument('--gen-augment', type=bool, default=False)
 args = parser.parse_args()
@@ -22,18 +23,18 @@ if args.prompt == -1:
 print(args)
 print('PROMPT :', prompts)
 
-BATCH_SIZE = 5
+BATCH_SIZE = args.bs
 MODEL_NAME = 'elmo'
 
 for p in prompts:
 
-    X_train, Y_train = data_utils.load_elmo_features(p, 'train')
-    X_val, Y_val = data_utils.load_elmo_features(p, 'val')
-    X_test, Y_test = data_utils.load_elmo_features(p, 'test')
+    train_df = data_utils.load_data(p, 'train')[:10]
+    val_df = data_utils.load_data(p, 'val')[:10]
+    # test_df = data_utils.load_data(p, 'test')
 
-    print(X_train.shape, Y_train.shape)
-    print(X_val.shape, Y_val.shape)
-    print(X_test.shape, Y_test.shape)
+    print(train_df.shape)
+    print(val_df.shape)
+    # print(test_df.shape)
 
     model = models.build_elmo_model_full(p)
 
@@ -43,15 +44,19 @@ for p in prompts:
         model.load_weights(last_weight)
     assert args.epoch > last_epoch
 
-    # train_gen = data_utils.elmo_gen(p, 'train', batch_size=BATCH_SIZE)
-    # val_gen = data_utils.elmo_gen(p, 'val', batch_size=BATCH_SIZE)
+    train_gen = data_utils.elmo_gen(p, train_df, batch_size=BATCH_SIZE)
+    val_gen = data_utils.elmo_gen(p, val_df, batch_size=BATCH_SIZE)
 
-    # train_steps = 1
-    # val_steps = 1
+    train_steps = np.ceil(len(train_df) / BATCH_SIZE)
+    val_steps = np.ceil(len(val_df) / BATCH_SIZE)
+
+    print(train_steps, val_steps)
 
     callbacks = [ModelCheckpoint(os.path.join(weight_path, 'weight.{}_{}_{{epoch:02d}}_{{val_loss:.4f}}.h5'.format(MODEL_NAME, p)), save_weights_only=True, period=1),
                  CSVLogger(os.path.join(
                      weight_path, 'history.csv'), append=True),
-                 eval_utils.EvaluateCallback(X_val, Y_val, MODEL_NAME, p, BATCH_SIZE)]
-    model.fit(X_train, Y_train, batch_size=BATCH_SIZE, epochs=args.epoch,
-              initial_epoch=last_epoch, callbacks=callbacks, validation_data=(X_val, Y_val))
+                 eval_utils.EvaluateCallback(p, val_df, MODEL_NAME, BATCH_SIZE)]
+    model.fit_generator(train_gen, steps_per_epoch=train_steps,
+                        validation_data=val_gen, validation_steps=val_steps,
+                        epochs=args.epoch, initial_epoch=last_epoch,
+                        callbacks=callbacks)
