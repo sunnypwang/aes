9f9ef00bb858ff86b501aba58a8973206516ce88
Phakawat Wangkriangkri <mariosunshinekirby@gmail.com>
2020-06-03 13:42:39 +0700

    added augment gen

--- data_utils.py
+++ data_utils.py
@@ -2,11 +2,10 @@ import numpy as np
 import pandas as pd
 import re
 from nltk.tokenize import sent_tokenize, word_tokenize
-from keras.preprocessing.sequence import pad_sequences
 
 import utils
 
-augment_set = ['no_art', 'no_conj', 'add_and_0.1', 'swap_0.05',
+augment_set = ['no_art', 'no_conj', 'add_and-0.1', 'swap_word-0.05',
                'no_first_sent', 'no_last_sent', 'no_longest_sent', 'reverse_sent']
 
 
@@ -58,7 +57,7 @@ def clean_text(text):
     text = text.lower()
     # Remove quotation
     text = re.sub(r'\"', '', text)
-    # URL replace
+    # URL replace by https://github.com/feidong1991/aes
     text = re.sub(
         '(http[s]?://)?((www)\.)?([a-zA-Z0-9]+)\.{1}((com)(\.(cn))?|(org))', '<url>', text)
     # Truncate any duplicate non-alphanumeric and add a space after it
@@ -67,6 +66,7 @@ def clean_text(text):
 
     # Remove extra whitespaces
     text = re.sub(r'\s+', ' ', text)
+
     return text
 
 
@@ -94,14 +94,20 @@ def sentenize(text):
 #             MAX_LEN[prompt] = len(essay)
 
 
-def prepare_elmo_features(df, prompt, features='essay', labels='domain1_score', x_only=False, pad=True, y_only=False, norm=True):
+def prepare_elmo_features(df, prompt, features='essay', labels='domain1_score', x_only=False, pad=True, y_only=False, norm=True, augment=None, rnd=None):
     assert not (x_only and y_only)
     if not y_only:
         X = []
+        # Sentence tokenize and make augment
         for essay in df[features]:
-            X.append(sentenize(essay))
+            sents = sentenize(essay)
+            if augment:
+                X.append(make_augment(sents, augment, rnd))
+            else:
+                X.append(sents)
         X = np.array(X)
         if pad:
+            from keras.preprocessing.sequence import pad_sequences
             X = pad_sequences(X, maxlen=MAXLEN[prompt], dtype=object,
                               padding='post', truncating='post', value=PAD_SENT_TOKEN)[:, :, None]
         if x_only:
@@ -128,25 +134,103 @@ def load_data(prompt, suffix=None, fold=1):
     return data
 
 
-def load_elmo_features(prompt, suffix=None, fold=1, x_only=False, pad=True, y_only=False, norm=True):
+def load_elmo_features(prompt, suffix=None, fold=1, **kwargs):
     data = load_data(prompt, suffix, fold)
-    X, Y = prepare_elmo_features(
-        data, prompt, x_only=x_only, pad=pad, y_only=y_only, norm=norm)
-    return X, Y
+    return prepare_elmo_features(
+        data, prompt, **kwargs)
 
 
-def elmo_gen(prompt, df, batch_size=1, test=False):
+def elmo_gen(prompt, df, batch_size=1, test=False, **kwargs):
     data = df.copy()
     while True:
         for i in range(0, len(data), batch_size):
             j = min(len(data), i+batch_size)
             if test:
-                x = prepare_elmo_features(data[i:j], prompt, x_only=True)
+                x = prepare_elmo_features(
+                    data[i:j], prompt, x_only=True, **kwargs)
                 yield x
             else:
-                x, y = prepare_elmo_features(data[i:j], prompt)
+                x, y = prepare_elmo_features(data[i:j], prompt, **kwargs)
                 yield x, y
 
+
+def augment_gen(prompt, test_df, batch_size=1, augment=None, **kwargs):
+    data = test_df.copy()
+    rnd = np.random.RandomState(1)
+    while True:
+        for i in range(0, len(data), batch_size):
+            j = min(len(data), i+batch_size)
+
+            x = prepare_elmo_features(
+                data[i:j], prompt, x_only=True, augment=augment, rnd=rnd, **kwargs)
+            yield x
+
+
+def make_augment(sents, augment, rnd=None):
+    '''augment essay (list of sentences)'''
+    assert augment in augment_set
+    t = augment.split('-')
+    if len(t) > 1:
+        augment, threshold = t[0], float(t[1])
+    else:
+        threshold = 1.0
+
+    new_sents = []
+    if not rnd:
+        rnd = np.random.RandomState(1)
+
+    if augment == 'no_art':
+        for sent in sents:
+            new_sents.append(re.sub(r'\b(a|an|the)\b ', r'', sent))
+
+    elif augment == 'no_conj':
+        for sent in sents:
+            new_sents.append(re.sub(r'\b(and|or|but)\b ', r'', sent))
+
+    elif augment == 'add_and':
+        for sent in sents:
+            state = rnd.rand()
+            if state < threshold:
+                sent = 'and ' + sent
+            new_sents.append(sent)
+
+    elif augment == 'swap_word':
+        for sent in sents:
+            words = sent.split()
+            word_idx = np.arange(len(words)-2)
+            rnd.shuffle(word_idx)
+            for i in word_idx:
+                state = rnd.rand()
+                if state < threshold:
+                    words[i], words[i+1] = words[i+1], words[i]
+            new_sents.append(' '.join(words))
+
+    elif augment == 'no_first_sent':
+        if len(sents) > 1:
+            new_sents.extend(sents[1:])
+        else:
+            new_sents.extend(['.'])
+
+    elif augment == 'no_last_sent':
+        if len(sents) > 1:
+            new_sents.extend(sents[:-1])
+        else:
+            new_sents.extend(['.'])
+
+    elif augment == 'no_longest_sent':
+        if len(sents) > 1:
+            maxidx = np.argmax([len(sent) for sent in sents])
+            new_sents.extend(sents[:maxidx] + sents[maxidx+1:])
+        else:
+            new_sents.extend(['.'])
+
+    elif augment == 'reverse_sent':
+        new_sents.extend(sents[::-1])
+
+    else:
+        raise NameError('Unknown augment : ' + str(augment))
+    assert type(new_sents) is list
+    return new_sents
 
 # def load_data(prompt_id, fold_id, suffix):
 #     path = 'prompt_{}/new/fold_{}/prompt_{}_{}.tsv'.format(
